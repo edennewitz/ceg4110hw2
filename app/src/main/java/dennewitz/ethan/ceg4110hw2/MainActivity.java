7ebfa21fd4fa6736522338491c0533c7d94151ed
Ethan <dennewitz.3@wright.edu>
2018-10-12 19:11:19 -0400

    hi

--- app/src/main/java/dennewitz/ethan/ceg4110hw2/MainActivity.java
+++ app/src/main/java/dennewitz/ethan/ceg4110hw2/MainActivity.java
@@ -1,7 +1,9 @@
 package dennewitz.ethan.ceg4110hw2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     public Button undoButton;
     public Button redoButton;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,17 +35,79 @@ public class MainActivity extends AppCompatActivity {
         monthButton = findViewById(R.id.monthButton);
         yearButton = findViewById(R.id.yearButton);
         analogButton = findViewById(R.id.analogButton);
-        digitalButton = findViewById(R.id.digitalButton); 
+        digitalButton = findViewById(R.id.digitalButton);
         secondButton = findViewById(R.id.secondButton);
         undoButton = findViewById(R.id.undoButton);
         redoButton = findViewById(R.id.redoButton);
 
+        secondButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
+        minuteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        hourButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        dayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        monthButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        yearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        undoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        redoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        digitalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDigitalClock();
+            }
+        });
+        analogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
 
 
     }
-
+    public void openDigitalClock(){
+        Intent intent = new Intent(this, digitalClock.class);
+        startActivity(intent);
+    }
     class Time{
         int second;
         int minute;
@@ -64,7 +129,7 @@ public class MainActivity extends AppCompatActivity {
     class changeSec implements Command{
         Time time ;
 
-        public void changeSec(Time time){
+        public void changeSecCommand(Time time){
 
         }
         public void execute(){
@@ -118,6 +183,77 @@ public class MainActivity extends AppCompatActivity {
         public void execute(){
 
         }
+    }
+
+    class currentTime {
+        int second;
+        int minute;
+        int hour;
+        int day;
+        String month;
+        int year;
+
+        public int getSecond()
+        {
+            return 0;
+        }
+
+        public void setSecond()
+        {
+
+        }
+
+        public int getMinute()
+        {
+            return 0;
+        }
+
+        public void setMinute()
+        {
+
+        }
+
+        public int getHour()
+        {
+            return 0;
+        }
+
+        public void setHour()
+        {
+
+        }
+
+        public int getDay()
+        {
+            return 0;
+        }
+
+        public void setDay()
+        {
+
+        }
+
+        public String getMonth()
+        {
+            return "";
+        }
+
+        public void setMonth()
+        {
+
+        }
+
+        public int getYear()
+        {
+            return 0;
+        }
+
+        public void setYear()
+        {
+
+        }
+
+
     }
 
 }
--- app/src/main/java/dennewitz/ethan/ceg4110hw2/digitalClock.java
+++ app/src/main/java/dennewitz/ethan/ceg4110hw2/digitalClock.java
@@ -2,12 +2,37 @@ package dennewitz.ethan.ceg4110hw2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class digitalClock extends AppCompatActivity {
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_digital_clock);
+
+
+        Calendar c = Calendar.getInstance();
+        System.out.println("Current time => "+c.getTime());
+
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String formattedDate = df.format(c.getTime());
+        // formattedDate have current date/time
+        Toast.makeText(this, formattedDate, Toast.LENGTH_SHORT).show();
+
+
+        TextView txtView = new TextView(this);
+        txtView.setText(formattedDate);
+        txtView.setGravity(Gravity.CENTER);
+        txtView.setTextSize(20);
+        setContentView(txtView);
     }
 }
